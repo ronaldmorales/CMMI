45311d9afbcff5e2628c8e6c95fbccc2772973a7
ronaldmorales <morales.ronald.1095@gmail.com>
2025-06-15 18:09:30 -0600

    Update process_flow.py

--- process_flow.py
+++ process_flow.py
@@ -1,22 +1,3 @@
-Iniciando ciclo de vida del microservivio:
-Fase actual: Análisis de requisitos
-Fase actual: Diseño técnico
-Fase actual: Implementación 
-Fase actual: Pruebas
-Fase actual: Integración
-Fase actual: Entrega
-
-
-
-
-
-
-
-
-
-
-
-
 # process_flow.py
 
 def ciclo_vida_microservicio():
